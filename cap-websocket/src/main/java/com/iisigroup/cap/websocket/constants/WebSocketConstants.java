6794de78e9f6487cc88fc3348f480f3715e43eb3
cathychang <>
2018-02-12 16:14:33 +0800

    [refs #6128] [cap-websocket] remove cap-websocket module

--- cap-websocket/src/main/java/com/iisigroup/cap/websocket/constants/WebSocketConstants.java
+++
@@ -1,7 +0,0 @@
-package com.iisigroup.cap.websocket.constants;
-
-import com.iisigroup.cap.constants.Constants;
-
-public interface WebSocketConstants extends Constants {
-    String SOCKET_CLIENT = "SocketClient";
-}
