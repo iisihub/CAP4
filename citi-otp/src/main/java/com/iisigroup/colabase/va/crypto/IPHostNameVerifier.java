d49b3bfa0d52a344d30b6be83bf9f4811b9dca2f
cathychang <cathychang307@gmail.com>
2018-03-27 10:16:56 +0800

    [COLA BASE] [citi-otp] add otp IP Host Name Verifier

---
+++ citi-otp/src/main/java/com/iisigroup/colabase/va/crypto/IPHostNameVerifier.java
@@ -0,0 +1,39 @@
+package com.iisigroup.colabase.va.crypto;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+
+/**
+ * IPHostName Verifier
+ * 
+ * @since Mar 14, 2018
+ * @author Cathy
+ * @version
+ *          <ul>
+ *          <li>Mar 14, 2018,Cathy,new
+ *          </ul>
+ */
+public class IPHostNameVerifier implements HostnameVerifier {
+
+    public boolean verify(String paramString, SSLSession paramSSLSession) {
+        if (paramString.compareTo(paramSSLSession.getPeerHost()) != 0) {
+            return false;
+        }
+        try {
+            String str1 = paramSSLSession.getPeerCertificateChain()[0].getSubjectDN().toString();
+            System.out.println(str1);
+            int i = str1.indexOf("CN=");
+            if (i == -1) {
+                return false;
+            }
+            String str2 = str1.substring(i + 3, str1.indexOf(',', i));
+            if (paramString.compareTo(str2) == 0) {
+                return true;
+            }
+        } catch (SSLPeerUnverifiedException localSSLPeerUnverifiedException) {
+            localSSLPeerUnverifiedException.printStackTrace();
+        }
+        return false;
+    }
+}
