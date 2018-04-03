5c1d3361ed94b170aa0c7135d8da09cbf2e13d9c
andychen <andy.chen@iisigroup.com>
2018-04-03 15:57:05 +0800

    [COLA BASE] [citi-ssl] add new client module

---
+++ citi-ssl/src/main/java/com/iisigroup/colabase/demo/sslclient/handler/SslClientHandler.java
@@ -0,0 +1,27 @@
+package com.iisigroup.colabase.demo.sslclient.handler;
+
+import com.iisigroup.cap.mvc.handler.MFormHandler;
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * <pre>
+ * demo use handler
+ * </pre>
+ *
+ * @author AndyChen
+ * @version
+ *          <ul>
+ *          <li>2018/04/03,AndyChen,new
+ *          </ul>
+ * @since 2018/03/12
+ */
+@Controller("demosslclienthandler")
+public class SslClientHandler extends MFormHandler {
+
+    public SslClientHandler() {
+        String sss = "";
+
+    }
+
+}
