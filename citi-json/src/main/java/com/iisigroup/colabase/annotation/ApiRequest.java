aa30890b1e3b266ed0e368ba574a91def0f6041e
andychen <andy.chen@iisigroup.com>
2018-05-15 11:57:50 +0800

    [COLA BASE] [citi-json] add annotation

---
+++ citi-json/src/main/java/com/iisigroup/colabase/annotation/ApiRequest.java
@@ -0,0 +1,22 @@
+package com.iisigroup.colabase.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author AndyChen
+ * @version <ul>
+ *          <li>2018/5/11 AndyChen,new
+ *          </ul>
+ * @since 2018/5/11
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiRequest {
+    /**
+     * path must follow format ex: person.name
+     * @return
+     */
+    String path() default "";
+    String defaultVaule() default "";
+    boolean noValueNoSend() default false;
+}
