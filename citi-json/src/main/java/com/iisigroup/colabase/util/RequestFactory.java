711d087647dbcc4460e86bfd819aed9fabe71ed0
andychen <andy.chen@iisigroup.com>
2018-05-15 11:57:11 +0800

    [COLA BASE] [citi-json] add util class

---
+++ citi-json/src/main/java/com/iisigroup/colabase/util/RequestFactory.java
@@ -0,0 +1,135 @@
+package com.iisigroup.colabase.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.iisigroup.colabase.annotation.ApiRequest;
+import com.iisigroup.colabase.annotation.JsonTemp;
+import com.iisigroup.colabase.model.RequestContent;
+import com.iisigroup.colabase.service.JsonDataService;
+import com.iisigroup.colabase.service.impl.JsonDataServiceImpl;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static jdk.nashorn.internal.objects.NativeString.substring;
+
+/**
+ * @author AndyChen
+ * @version
+ *          <ul>
+ *          <li>2018/5/11 AndyChen,new
+ *          </ul>
+ * @since 2018/5/11
+ */
+public class RequestFactory {
+
+    private static final String REQUEST_CONTENT_KEY = "requestContent";
+    private static final String NO_SEND_LIST_KEY = "noSendList";
+
+    static {
+        jsonDataService = new JsonDataServiceImpl();
+    }
+
+    private static JsonDataService jsonDataService;
+
+    private RequestFactory() {
+
+    }
+
+    public static <T extends RequestContent> T getInstance(Class<T> requestClass) {
+        T instance;
+        try {
+            instance = RequestProxy.getInstance(requestClass);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalArgumentException("init instance exception. warp exception: " + e.getClass());
+        }
+        return instance;
+    }
+
+    static <T extends RequestContent> void initJsonObject(Class<T> requestClass, T instance) throws NoSuchFieldException, IllegalAccessException {
+        Field jsonObjField = RequestContent.class.getDeclaredField(REQUEST_CONTENT_KEY);
+        jsonObjField.setAccessible(true);
+        Field jsonStrField = null;
+        for (Field field : requestClass.getDeclaredFields()) {
+            JsonTemp annotation = field.getAnnotation(JsonTemp.class);
+            if (annotation != null) {
+                jsonStrField = field;
+                break;
+            }
+        }
+        if (jsonStrField == null)
+            throw new IllegalStateException("please check Request model defined jsonTemp with annotation @JsonTemp");
+        jsonStrField.setAccessible(true);
+        Object jsonStr = jsonStrField.get(instance);
+        Gson gson = new Gson();
+        //init jsonObject
+        JsonObject jsonObject = gson.fromJson(String.valueOf(jsonStr), JsonObject.class);
+        jsonObjField.set(instance, jsonObject);
+
+        Map<String, String> valueMap = new HashMap<>();
+        List<String> noSendList = getListToInstance(instance);
+        setDefaultValueMapAndNoSendList(valueMap, noSendList, instance.getClass());
+
+        // clean current jsonObject
+        jsonDataService.cleanJsonObjectData(instance);
+        // set default value to it
+        jsonDataService.setDefaultValue(instance, valueMap);
+        String s = "";
+    }
+
+    private static <T extends RequestContent> List<String> getListToInstance(T instance) {
+        try {
+            Field noSendList = RequestContent.class.getDeclaredField(NO_SEND_LIST_KEY);
+            noSendList.setAccessible(true);
+            return (List<String>) noSendList.get(instance);
+        } catch (NoSuchFieldException |IllegalAccessException e) {
+            throw new IllegalArgumentException("instance provide did not have list field");
+        }
+    }
+
+    private static void setDefaultValueMapAndNoSendList(Map<String, String> valueMap, List<String> noVnoSLinst, Class<?> tClass) {
+        if(tClass == RequestContent.class)
+            return;
+
+        Field[] fields = tClass.getDeclaredFields();
+        for (Field field : fields) {
+            ApiRequest annotation = field.getAnnotation(ApiRequest.class);
+            if (annotation == null) {
+                continue;
+            }
+            String path = annotation.path();
+            String defValue = annotation.defaultVaule();
+            boolean isNoSend = annotation.noValueNoSend();
+            valueMap.put(field.getName(), defValue);
+            if(isNoSend)
+                noVnoSLinst.add(path);
+        }
+        setDefaultValueMapAndNoSendList(valueMap, noVnoSLinst, tClass.getSuperclass());
+    }
+
+
+    static void setValueToJsonContent(Object object, Method method, Object[] args) {
+        String value = String.valueOf(args.length > 0 ? args[0] : "");
+        String methodName = method.getName();
+        String fieldName = methodNameToFieldName(methodName);
+        jsonDataService.setParamToJsonContent((RequestContent) object, fieldName, value);
+    }
+
+    public static Object getFieldObject(Object requestObj, String fieldName) throws NoSuchFieldException,
+            IllegalAccessException {
+        Class superclass = RequestContent.class;
+            Field reqContentField = superclass.getDeclaredField(fieldName);
+            reqContentField.setAccessible(true);
+            return reqContentField.get(requestObj);
+    }
+
+    private static String methodNameToFieldName(String methodName) {
+        String result = "";
+        result += methodName.substring(3, 4).toLowerCase();
+        result += methodName.substring(4);
+        return result;
+    }
+}
---
+++ citi-json/src/main/java/com/iisigroup/colabase/util/RequestProxy.java
@@ -0,0 +1,63 @@
+package com.iisigroup.colabase.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import com.iisigroup.colabase.annotation.JsonTemp;
+import com.iisigroup.colabase.model.RequestContent;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+/**
+ * @author AndyChen
+ * @version <ul>
+ *          <li>2018/5/11 AndyChen,new
+ *          </ul>
+ * @since 2018/5/11
+ */
+public class RequestProxy implements MethodInterceptor {
+
+    static {
+        proxy = new RequestProxy();
+    }
+
+    private static RequestProxy proxy;
+
+    private RequestProxy() {
+
+    }
+
+    static <T extends RequestContent> T getInstance(Class<T> requestClass) throws NoSuchFieldException,
+            IllegalAccessException {
+        if(!checkJsonTemp(requestClass)) {
+            throw new IllegalStateException("please check Request model defined jsonTemp with annotation @JsonTemp");
+        }
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(requestClass);
+        enhancer.setCallback(proxy);
+        T result = (T) enhancer.create();
+        RequestFactory.initJsonObject(requestClass, result);
+        return result;
+    }
+
+
+    @Override
+    public Object intercept(Object object, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        if(method.getName().contains("set")) {
+            RequestFactory.setValueToJsonContent(object, method, args);
+        }
+        return methodProxy.invokeSuper(object, args);
+    }
+
+    private static <T> boolean checkJsonTemp(Class<T> objClass) {
+        for (Field field : objClass.getDeclaredFields()) {
+            JsonTemp annotation = field.getAnnotation(JsonTemp.class);
+            if(annotation != null)
+                return true;
+        }
+        return false;
+    }
+
+
+}
