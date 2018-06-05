f2f649ee443b66ab83ec7b753a177af7c7506720
andychen <andy.chen@iisigroup.com>
2018-06-05 15:00:58 +0800

    [COLA BASE] [citi-address] remove no use code

--- citi-address/src/main/java/com/iisigroup/colabase/model/Address.java
+++ citi-address/src/main/java/com/iisigroup/colabase/model/Address.java
@@ -1,14 +1,5 @@
 package com.iisigroup.colabase.model;
 
 public class Address {
-    private String address;
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
 }
