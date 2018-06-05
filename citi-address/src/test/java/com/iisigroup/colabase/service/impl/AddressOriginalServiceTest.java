fda73977672bd4022a7235fb817289a98537c130
andychen <andy.chen@iisigroup.com>
2018-06-05 12:22:11 +0800

    [COLA BASE] [citi-address] add JUnit test for Spring

--- citi-address/src/test/java/com/iisigroup/colabase/service/impl/AddressOriginalServiceTest.java
+++ citi-address/src/test/java/com/iisigroup/colabase/service/impl/AddressOriginalServiceTest.java
@@ -1,22 +1,27 @@
 package com.iisigroup.colabase.service.impl;
 
+import com.iisigroup.colabase.service.AddressService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Map;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by VALLA on 2018/5/7.
  */
-public class AddressServiceImplTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/test_applicationContext.xml"})
+public class AddressOriginalServiceTest {
 
-
-
+    @Autowired
+    private AddressService addressService;
 
     @Test
     public void testAddress() throws Exception {
-        AddressServiceImpl addressService = new TestClass();
+
 //        String testAddress = "241新北市三重區中正南路149-2號9樓";
         String testAddress = "100臺北市中正區中華路１段149-2號9樓";
         Map<String, String> normalize = addressService.normalize(testAddress);
