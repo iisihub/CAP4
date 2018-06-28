ddcedecb5ee64c0818816ff7eed6fcaddf9a4abf
cathychang <cathychang307@gmail.com>
2018-06-28 10:52:33 +0800

    [refs #6719]  citi-pdf citi-edm CCBasePageReport IFreeMarkerReport -> 搬到citi-report

--- citi-report/src/main/java/com/iisigroup/colabase/report/CCBasePageReport.java
+++ citi-report/src/main/java/com/iisigroup/colabase/report/CCBasePageReport.java
@@ -9,7 +9,7 @@
  * This software is confidential and proprietary information of 
  * International Integrated System, Inc. (&quot;Confidential Information&quot;).
  */
-package com.iisigroup.colabase.pdf.report;
+package com.iisigroup.colabase.report;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
