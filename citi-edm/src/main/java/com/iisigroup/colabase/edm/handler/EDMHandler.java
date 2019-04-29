9c3c7188e553fe77c46f0c338b0ffaa491ca136a
JohnsonHo <gux2025@gmail.com>
2019-04-29 10:57:46 +0800

    [COLA BASE] [citi-edm] update for java doc

--- citi-edm/src/main/java/com/iisigroup/colabase/edm/handler/EDMHandler.java
+++ citi-edm/src/main/java/com/iisigroup/colabase/edm/handler/EDMHandler.java
@@ -24,25 +24,27 @@ import com.iisigroup.cap.mvc.handler.MFormHandler;
 import com.iisigroup.colabase.edm.model.EdmSetting;
 import com.iisigroup.colabase.edm.service.EDMService;
 
-/**<pre>
+/**
+ * <pre>
  * Demo EDM功能 handler
  * </pre>
- * @since  2018年4月30日
+ * 
+ * @since 2018年4月30日
  * @author Johnson Ho
- * @version <ul>
- *           <li>2018年4月30日,Johnson Ho,new
+ * @version
+ *          <ul>
+ *          <li>2018年4月30日,Johnson Ho,new
  *          </ul>
  */
 
 @Controller("demoedmhandler")
 public class EDMHandler extends MFormHandler {
-    
+
     @Autowired
     private EDMService edmService;
-    
+
     private static final String RESULT = "result";
 
-    
     /**
      * @param request
      * @return
@@ -54,37 +56,37 @@ public class EDMHandler extends MFormHandler {
         String edmCustomerName = null;
         String edmProject = null;
         try {
-              mailAddress = request.get("mailAddress");
-              edmFtlPath = request.get("edmFtlPath");
-              //對應ftl內的變數
-              edmCustomerName = request.get("edmCustomerName");
-              edmProject = request.get("edmProject");
-              
-              // 給EDM值
-              EdmSetting edmSetting = new EdmSetting();
-              edmSetting.setEdmFtlPath("report/" + edmFtlPath);
-              edmSetting.setMailAddress(mailAddress);
-              edmSetting.setFromAddress("citi@imta.citicorp.com");
-              edmSetting.setFromPerson("花旗（台灣）銀行");
-              edmSetting.setEdmHost("smtp.gmail.com");
-              edmSetting.setEdmUsr("css123456tw@gmail.com");
-              edmSetting.setEdmPwd("kvzulwkqdoiprtfb");
-              edmSetting.setEdmSubject("花旗(台灣)銀行 圓滿貸線上申請確認通知函");
-              edmSetting.setEdmAttachedFilePath("D:/COLA/圓滿貸線上申請.pdf");
-              edmSetting.setEdmImageFileFolder("D:/COLA/image");
-              
-              Map<String, Object> ftlVar = new HashMap<String, Object>();
-              ftlVar.put("otherAccountTitleMask", edmCustomerName);
-              ftlVar.put("otherAccountNumberMask", edmProject);
-              edmSetting.setMappingFtlVar(ftlVar);
-              
-              edmService.sendEDM(edmSetting);
-            
+            mailAddress = request.get("mailAddress");
+            edmFtlPath = request.get("edmFtlPath");
+            // 對應ftl內的變數
+            edmCustomerName = request.get("edmCustomerName");
+            edmProject = request.get("edmProject");
+
+            // 給EDM值
+            EdmSetting edmSetting = new EdmSetting();
+            edmSetting.setEdmFtlPath("report/" + edmFtlPath);
+            edmSetting.setMailAddress(mailAddress);
+            edmSetting.setFromAddress("citi@imta.citicorp.com");
+            edmSetting.setFromPerson("花旗（台灣）銀行");
+            edmSetting.setEdmHost("smtp.gmail.com");
+            edmSetting.setEdmUsr("css123456tw@gmail.com");
+            edmSetting.setEdmPwd("kvzulwkqdoiprtfb");
+            edmSetting.setEdmSubject("花旗(台灣)銀行 圓滿貸線上申請確認通知函");
+            edmSetting.setEdmAttachedFilePath("D:/COLA/圓滿貸線上申請.pdf");
+            edmSetting.setEdmImageFileFolder("D:/COLA/image");
+
+            Map<String, Object> ftlVar = new HashMap<String, Object>();
+            ftlVar.put("otherAccountTitleMask", edmCustomerName);
+            ftlVar.put("otherAccountNumberMask", edmProject);
+            edmSetting.setMappingFtlVar(ftlVar);
+
+            edmService.sendEDM(edmSetting);
+
         } catch (Exception e) {
             result.set(RESULT, "Fail, cause : " + e.getClass());
         }
         result.set(RESULT, "Success, EDM already send to : " + mailAddress);
         return result;
     }
-    
+
 }
--- citi-edm/src/main/java/com/iisigroup/colabase/edm/model/EdmSetting.java
+++ citi-edm/src/main/java/com/iisigroup/colabase/edm/model/EdmSetting.java
@@ -13,43 +13,70 @@ package com.iisigroup.colabase.edm.model;
 
 import java.util.Map;
 
-/**<pre>
+/**
  * EDM相關設定參數
- * </pre>
- * @since  2019年3月12日
+ * 
+ * @since 2019年3月12日
  * @author Johnson Ho
- * @version <ul>
- *           <li>2019年3月12日,Johnson Ho,new
+ * @version
+ *          <ul>
+ *          <li>2019年3月12日,Johnson Ho,new
  *          </ul>
  */
 public class EdmSetting {
-    
+
+    /** FTL的檔案路徑 */
     private String edmFtlPath;
+    /** 收件人email */
     private String mailAddress;
+    /** 寄件者email */
     private String fromAddress;
+    /** 寄件者名稱 */
     private String fromPerson;
+    /** SMTP host */
     private String edmHost;
+    /** 帳號 */
     private String edmUsr;
+    /** 密碼 */
     private String edmPwd;
+    /** 信件主旨 */
     private String edmSubject;
+    /** 附件的檔案路徑 */
     private String edmAttachedFilePath;
+    /** 對應FTL內的image"資料夾"路徑 */
     private String edmImageFileFolder;
+    /** 對應FTL內的變數Map */
     private Map<String, Object> mappingFtlVar;
-    
+
+    /**
+     * EdmSetting empty constructor
+     */
     public EdmSetting() {
     }
+
     /**
-     * @param edmFtlPath FTL的檔案路徑
-     * @param mailAddress 收件人email
-     * @param fromAddress 寄件者email
-     * @param fromPerson 寄件者名稱
-     * @param edmHost SMTP host name
-     * @param edmUsr 帳號
-     * @param edmPwd 密碼
-     * @param edmSubject 信件主旨
-     * @param edmAttachedFilePath 附件的檔案路徑
-     * @param edmImageFileFolder 對應FTL內的image"資料夾"路徑
-     * @param mappingFtlVar 對應FTL內的變數Map
+     * @param edmFtlPath
+     *            FTL的檔案路徑
+     * @param mailAddress
+     *            收件人email
+     * @param fromAddress
+     *            寄件者email
+     * @param fromPerson
+     *            寄件者名稱
+     * @param edmHost
+     *            SMTP host
+     * @param edmUsr
+     *            帳號
+     * @param edmPwd
+     *            密碼
+     * @param edmSubject
+     *            信件主旨
+     * @param edmAttachedFilePath
+     *            附件的檔案路徑
+     * @param edmImageFileFolder
+     *            對應FTL內的image"資料夾"路徑
+     * @param mappingFtlVar
+     *            對應FTL內的變數Map
      */
     public EdmSetting(String edmFtlPath, String mailAddress, String fromAddress, String fromPerson, String edmHost, String edmUsr, String edmPwd, String edmSubject, String edmAttachedFilePath,
             String edmImageFileFolder, Map<String, Object> mappingFtlVar) {
@@ -65,137 +92,214 @@ public class EdmSetting {
         this.edmImageFileFolder = edmImageFileFolder;
         this.mappingFtlVar = mappingFtlVar;
     }
+
     /**
+     * Get the edmFtlPath
+     * 
      * @return the edmFtlPath
      */
     public String getEdmFtlPath() {
         return edmFtlPath;
     }
+
     /**
-     * @param edmFtlPath the edmFtlPath to set
+     * Set the edmFtlPath
+     * 
+     * @param edmFtlPath
+     *            the edmFtlPath to set
      */
     public void setEdmFtlPath(String edmFtlPath) {
         this.edmFtlPath = edmFtlPath;
     }
+
     /**
+     * Get the mailAddress
+     * 
      * @return the mailAddress
      */
     public String getMailAddress() {
         return mailAddress;
     }
+
     /**
-     * @param mailAddress the mailAddress to set
+     * Set the mailAddress
+     * 
+     * @param mailAddress
+     *            the mailAddress to set
      */
     public void setMailAddress(String mailAddress) {
         this.mailAddress = mailAddress;
     }
+
     /**
+     * Get the fromAddress
+     * 
      * @return the fromAddress
      */
     public String getFromAddress() {
         return fromAddress;
     }
+
     /**
-     * @param fromAddress the fromAddress to set
+     * Set the fromAddress
+     * 
+     * @param fromAddress
+     *            the fromAddress to set
      */
     public void setFromAddress(String fromAddress) {
         this.fromAddress = fromAddress;
     }
+
     /**
+     * Get the fromPerson
+     * 
      * @return the fromPerson
      */
     public String getFromPerson() {
         return fromPerson;
     }
+
     /**
-     * @param fromPerson the fromPerson to set
+     * Set the fromPerson
+     * 
+     * @param fromPerson
+     *            the fromPerson to set
      */
     public void setFromPerson(String fromPerson) {
         this.fromPerson = fromPerson;
     }
+
     /**
+     * Get the edmHost
+     * 
      * @return the edmHost
      */
     public String getEdmHost() {
         return edmHost;
     }
+
     /**
-     * @param edmHost the edmHost to set
+     * Set the edmHost
+     * 
+     * @param edmHost
+     *            the edmHost to set
      */
     public void setEdmHost(String edmHost) {
         this.edmHost = edmHost;
     }
+
     /**
+     * Get the edmUsr
+     * 
      * @return the edmUsr
      */
     public String getEdmUsr() {
         return edmUsr;
     }
+
     /**
-     * @param edmUsr the edmUsr to set
+     * Set the edmUsr
+     * 
+     * @param edmUsr
+     *            the edmUsr to set
      */
     public void setEdmUsr(String edmUsr) {
         this.edmUsr = edmUsr;
     }
+
     /**
+     * Get the edmPwd
+     * 
      * @return the edmPwd
      */
     public String getEdmPwd() {
         return edmPwd;
     }
+
     /**
-     * @param edmPwd the edmPwd to set
+     * Set the edmPwd
+     * 
+     * @param edmPwd
+     *            the edmPwd to set
      */
     public void setEdmPwd(String edmPwd) {
         this.edmPwd = edmPwd;
     }
+
     /**
+     * Get the edmSubject
+     * 
      * @return the edmSubject
      */
     public String getEdmSubject() {
         return edmSubject;
     }
+
     /**
-     * @param edmSubject the edmSubject to set
+     * Set the edmSubject
+     * 
+     * @param edmSubject
+     *            the edmSubject to set
      */
     public void setEdmSubject(String edmSubject) {
         this.edmSubject = edmSubject;
     }
+
     /**
+     * Get the edmAttachedFilePath
+     * 
      * @return the edmAttachedFilePath
      */
     public String getEdmAttachedFilePath() {
         return edmAttachedFilePath;
     }
+
     /**
-     * @param edmAttachedFilePath the edmAttachedFilePath to set
+     * Set the edmAttachedFilePath
+     * 
+     * @param edmAttachedFilePath
+     *            the edmAttachedFilePath to set
      */
     public void setEdmAttachedFilePath(String edmAttachedFilePath) {
         this.edmAttachedFilePath = edmAttachedFilePath;
     }
+
     /**
+     * Get the edmImageFileFolder
+     * 
      * @return the edmImageFileFolder
      */
     public String getEdmImageFileFolder() {
         return edmImageFileFolder;
     }
+
     /**
-     * @param edmImageFileFolder the edmImageFileFolder to set
+     * Set the edmImageFileFolder
+     * 
+     * @param edmImageFileFolder
+     *            the edmImageFileFolder to set
      */
     public void setEdmImageFileFolder(String edmImageFileFolder) {
         this.edmImageFileFolder = edmImageFileFolder;
     }
+
     /**
+     * Get the mappingFtlVar
+     * 
      * @return the mappingFtlVar
      */
     public Map<String, Object> getMappingFtlVar() {
         return mappingFtlVar;
     }
+
     /**
-     * @param mappingFtlVar the mappingFtlVar to set
+     * Set the mappingFtlVar
+     * 
+     * @param mappingFtlVar
+     *            the mappingFtlVar to set
      */
     public void setMappingFtlVar(Map<String, Object> mappingFtlVar) {
         this.mappingFtlVar = mappingFtlVar;
     }
-    
+
 }
