9d1cb7ea43fc565265d1fa125975cea26d1ea777
andychen <andy.chen@iisigroup.com>
2019-07-19 17:38:57 +0800

    [COLA BASE] [citi-ssl] [citi-json] add comment for newbie

--- citi-json/src/main/java/com/iisigroup/colabase/json/annotation/ApiRequest.java
+++ citi-json/src/main/java/com/iisigroup/colabase/json/annotation/ApiRequest.java
@@ -17,7 +17,19 @@ public @interface ApiRequest {
      * @return
      */
     String path() default "";
+
+    /**
+     * 預設要送出的值
+     */
     String defaultValue() default "";
+
+    /**
+     * 在應用階段field沒有值，且該設定為true，則會刪除該欄位。
+     */
     boolean noValueNoSend() default false;
+
+    /**
+     * 標示到的屬性，如果在應用階段field沒有值，且該設定為true，則會刪除同階層欄位。
+     */
     boolean primaryEmptyClean() default false;
 }
